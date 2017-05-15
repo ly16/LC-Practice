849519c4b2a4967ba9250cfc59a178f23c9582dc
Lili Yu <liliyu3@hotmail.com>
2017-05-14 22:20:24 -0400

    Create  setter and getter.java

---
+++ setter and getter.java
@@ -0,0 +1,6 @@
+/*
+Implement a class School, including the following attributes and methods:
+
+A private attribute name of type string.
+A setter method setName which expect a parameter name of type string.
+A getter method getName which expect no parameter and return the name of the school.*/
