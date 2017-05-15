00b9f1a53678413b50465eb4865993cd2fe55038
Lili Yu <liliyu3@hotmail.com>
2017-05-14 22:22:40 -0400

    Update setter and getter.java

--- setter and getter.java
+++ setter and getter.java
@@ -3,4 +3,23 @@ Implement a class School, including the following attributes and methods:
 
 A private attribute name of type string.
 A setter method setName which expect a parameter name of type string.
-A getter method getName which expect no parameter and return the name of the school.*/
+A getter method getName which expect no parameter and return the name of the school.
+eg.
+School school = new School();
+school.setName("MIT");
+school.getName(); // should return "MIT" as a result.
+*/
+public class School {
+
+    private String name;
+    public void setName(String name)
+    {
+        this.name=name;//refer to any member of the current object from within an instance method or a constructor by using this
+    }
+   
+    
+     public String getName()
+     {
+         return this.name;
+     }
+}
