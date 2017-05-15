ef72b2a8a470486c24f3dc161c0c72aadc59699c
Lili Yu <liliyu3@hotmail.com>
2017-05-15 00:16:07 -0400

    Create String to Integer.java

---
+++ String to Integer.java
@@ -0,0 +1,10 @@
+/*Given a string, convert it to an integer. You may assume the string is a valid integer number that can be presented by
+a signed 32bit integer (-231 ~ 231-1).*/
+
+
+public class Solution {
+
+    public int stringToInteger(String str) {
+        return Integer.valueOf(str);//use the function to change variable type
+    }
+}
