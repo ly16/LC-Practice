eecda4056caf106213b4f2da1f2b577e2df53189
Lili Yu <liliyu3@hotmail.com>
2017-05-19 19:13:34 -0400

    Update Merge Two Sorted Lists.java

--- Merge Two Sorted Lists.java
+++ Merge Two Sorted Lists.java
@@ -23,7 +23,7 @@ public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         // write your code here
         ListNode dummy= new ListNode(0);
-        ListNode lastNode=dummy;
+        ListNode lastNode=dummy;            //lastNode will move from dummy to the end of list     
         while(l1!=null && l2!=null)
         {
             if(l1.val<l2.val)
