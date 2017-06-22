2f85c3801ee93e7e9f361c85315323281ccc9732
Lili Yu <liliyu3@hotmail.com>
2017-06-22 00:16:39 -0400

    Create Insertion Sort List.java

---
+++ Insertion Sort List.java
@@ -0,0 +1,38 @@
+/*
+Sort a linked list using insertion sort.
+Example
+Given 1->3->2->0->null, return 0->1->2->3->null.
+*/
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @return: The head of linked list.
+     */
+    public ListNode insertionSortList(ListNode head) {
+        // write your code here
+       ListNode sentinel = new ListNode(0);
+       while(head!=null){
+           ListNode cur = sentinel;
+           while(cur.next!=null && cur.next.val < head.val){
+               cur=cur.next;
+           }
+           // exchange orders
+           ListNode t= head.next;
+           head.next= cur.next;
+           cur.next= head;
+           head=t;
+       }
+       return sentinel.next;
+    }
+}
