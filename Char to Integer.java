881a26652c25ac8f2aca6698425acfa3486d5dfb
Lili Yu <liliyu3@hotmail.com>
2017-07-02 23:06:53 -0400

    Create Char to Integer.java

---
+++ Char to Integer.java
@@ -0,0 +1,18 @@
+/*
+Convert a char to an integer. 
+You can assume the char is in ASCII code (See Definition, so the value of the char should be in 0~255.
+
+Example
+Given a, return 97.
+Given %, return 37.
+*/
+public class Solution {
+    /**
+     * @param character a character
+     * @return an integer
+     */
+    public int charToInteger(char character) {
+        // Write your code here
+        return (int) character;
+    }
+}
