3705b9d3f8735d180f096bcb8e8b5968f8acbf78
Lili Yu <liliyu3@hotmail.com>
2017-07-07 23:08:43 -0400

    Create Lowercase to Uppercase.java

---
+++ Lowercase to Uppercase.java
@@ -0,0 +1,16 @@
+/*
+Convert a lowercase character to uppercase.
+Example
+a -> A, b -> B ...
+*/
+
+public class Solution {
+    /**
+     * @param character a character
+     * @return a character
+     */
+    public char lowercaseToUppercase(char character) {
+        // Write your code here
+        return Character.toUpperCase(character);
+    }
+}
