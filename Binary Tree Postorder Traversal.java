1a1a084b5249251400cb560db6cfe26f08c648cb
Lili Yu <liliyu3@hotmail.com>
2017-07-12 22:58:50 -0400

    Create Binary Tree Postorder Traversal.java

---
+++ Binary Tree Postorder Traversal.java
@@ -0,0 +1,42 @@
+/*
+Given a binary tree, return the postorder traversal of its nodes' values.
+
+Example
+Given binary tree {1,#,2,3},
+   1
+    \
+     2
+    /
+   3
+
+*/
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: Postorder in ArrayList which contains node values.
+     */
+    public ArrayList<Integer> postorderTraversal(TreeNode root) {
+        // write your code here
+        ArrayList<Integer> result= new ArrayList<>();
+        if(root==null){
+            return result;
+        }
+        result.addAll(postorderTraversal(root.left));
+        result.addAll(postorderTraversal(root.right));
+        result.add(root.val);
+        
+        return result;
+    }
+}
