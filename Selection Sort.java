7d0425765ba505e4d048d062d6bc39356766226a
Lili Yu <liliyu3@hotmail.com>
2017-08-04 15:16:41 -0400

    Update Selection Sort.java

--- Selection Sort.java
+++ Selection Sort.java
@@ -2,6 +2,7 @@
 Given an array of integers, sort the elements in the array in ascending order. 
 The selection sort algorithm should be used to solve this problem.
 time=O(n^2),extra space=O(1)
+
 Examples
 {1} is sorted to {1}
 {4, 2, -3, 6, 1} is sorted to {-3, 1, 2, 4, 6}
