cb24cf199097ac9cc5ab3b5076f69165c0eb2b06
Lili Yu <liliyu3@hotmail.com>
2017-08-13 21:30:19 -0400

    Create K Closest In Sorted Array.java

---
+++ K Closest In Sorted Array.java
@@ -0,0 +1,72 @@
+/*
+Given a target integer T, a non-negative integer K and an integer array A sorted in ascending order, 
+find the K closest numbers to T in A.
+time = O(logn + k)
+
+A size K integer array containing the K closest numbers(not indices) in A, 
+sorted in ascending order by the difference between the number and T. 
+
+A = {1, 4, 6, 8}, T = 3, K = 3, return {4, 1, 6}
+steps:
+1 4 6 8
+l m   r
+1 4 6 8
+l r
+4 > 3 but 1 < 3
+so left = 0, right = 1;
+3 - 1 > 4 - 3 so return 4
+1 4 6 8
+l   r
+3 - 1 < 6 - 3 so return 1
+  1 4 6 8
+l     r
+l = -1 < 0 so just move the right side 6 in the array
+
+*/
+
+public class Solution {
+  public int[] kClosest(int[] array, int target, int k) {
+    // Write your solution here
+    if (array == null || array.length == 0){
+      return array;
+    }
+    if (k == 0){
+      return new int[0];
+    }
+    int left = noMoreThan(array, target);
+    int right = left + 1;
+    
+    int[] result = new int[k];
+    for (int i = 0; i < k; i++){
+      //right out of bound; both in the bound, left close to the target, return left
+      if (right >= array.length || left >= 0 && target - array[left] <= array[right]-target){
+        result[i] = array[left--];
+      } else {  //if left < 0, right == 0, always right ++
+        result[i] = array[right++];
+      }
+    }
+    return result;
+  }
+  
+  private int noMoreThan(int[] array, int target){
+    int left = 0;
+    int right = array.length - 1;
+    // find the number no more than target return index, if not find return -1
+    while (left + 1 < right){
+      int mid = left + (right - left) / 2;
+      if (array[mid] <= target){
+        left = mid;
+      } else {
+        right = mid;
+      }
+    }
+    
+    if(array[right] <= target){
+      return right;
+    }
+    if (array[left] <= target){
+      return left;
+    }
+    return -1;
+  }
+}
