66904433d7d537dc7ac9540295db5710c65fa96f
Lili Yu <liliyu3@hotmail.com>
2017-12-12 15:13:14 -0500

    Update A to the power of b.java

--- A to the power of b.java
+++ A to the power of b.java
@@ -27,6 +27,6 @@ public class Solution {
     }
     // Recursion
     long half = power(a, b / 2);
-    return (b % 2 == 0) ? half * half : half * half *a;
+    return b % 2 == 0 ? half * half : half * half *a;
   }
 }
