b19aed3ed1352a59ed391fd9ebaa861353104bf2
Lili Yu <liliyu3@hotmail.com>
2017-12-12 15:48:46 -0500

    Update First Occurrence.java

--- First Occurrence.java
+++ First Occurrence.java
@@ -13,32 +13,30 @@ l m r
 l r
 check array[left] and array[right]
 
+time = O(log(n))
+space = O(1)
 */
 
 public class Solution {
   public int firstOccur(int[] array, int target) {
     // Write your solution here
-    if (array == null || array.length == 0){
+    if (array == null || array.length == 0) {
       return -1;
     }
     int left = 0;
     int right = array.length - 1;
-    while (left + 1 < right){   // leave two numbers
+    while (left + 1 < right) {
       int mid = left + (right - left) / 2;
-      if (array[mid] == target){
+      if (array[mid] >= target) {
         right = mid;
-      } else if (array[mid] > target){
-        right = mid - 1;        // or right = mid
       } else {
-        left = mid + 1;         // or left = mid
+        left = mid;
       }
     }
     
-    // check left side first for the first occurrence
-    if (array[left] == target){
+    if (array[left] == target) {
       return left;
-    }
-    if (array[right] == target){
+    } else if (array[right] ==  target) {
       return right;
     }
     return -1;
