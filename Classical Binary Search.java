64438639deeb625ebca2c2e9116e63b19808705f
Lili Yu <liliyu3@hotmail.com>
2017-12-12 15:32:10 -0500

    Update Classical Binary Search.java

--- Classical Binary Search.java
+++ Classical Binary Search.java
@@ -7,36 +7,28 @@ For target = 2, return 1 or 2.
 For target = 5, return 4 or 5.
 For target = 6, return -1.
 
+time = O(log(n))
+space = O(1)
 */
+
 public class Solution {
-    /**
-     * @param nums: An integer array sorted in ascending order
-     * @param target: An integer
-     * @return an integer
-     */
-    public int findPosition(int[] nums, int target) {
-        // Write your code here
-        if(nums==null || nums.length==0){
-            return -1;
-        }
-        int start=0, end=nums.length-1;
-        if(nums[start]==target){
-            return start;
-        }
-        if(nums[end]==target){
-            return end;
-        }
-        
-        while(start+1<end){
-            int mid=start+(end-start)/2;
-            if(nums[mid]==target){
-                return mid;
-            }else if(nums[mid]<target){
-                start=mid;
-            }else{
-                end=mid;
-            }
-        }
-        return -1;
+  public int binarySearch(int[] array, int target) {
+    if (array == null || array.length == 0) {
+      return -1;
     }
+    int left = 0;
+    int right = array.length - 1;
+    while (left <= right) {
+      int mid = left + (right - left);
+      if (array[mid] == target) {
+        return mid;
+      } else if (array[mid] > target) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+    return -1;
+  }
 }
+
