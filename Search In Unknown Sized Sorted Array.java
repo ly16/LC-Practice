b7fdd5f160ae2f265ad0e7e2ba17ba2fb4fbca64
Lili Yu <liliyu3@hotmail.com>
2017-12-12 22:59:43 -0500

    Update Search In Unknown Sized Sorted Array.java

--- Search In Unknown Sized Sorted Array.java
+++ Search In Unknown Sized Sorted Array.java
@@ -40,17 +40,37 @@ space = O(1)
 public class Solution {
   public int search(Dictionary dict, int target) {
     // Write your solution here
-    if(dict == null){
+    if (dict == null) {
       return -1;
     }
     int left = 0;
     int right = 1;
-    while(dict.get(right) != null && dict.get(right) < target){
+    while (dict.get(right) != null && dict.get(right) < target) {
       left = right;
-      right = right * 2;
+      right = 2 * right;
     }
     return binarySearch(dict, target, left, right);
   }
+  
+  private int binarySearch(Dictionary dict, int target, int left, int right) {
+    // the right pointer may be null, so you cannot get(mid), since mid is null
+    while(left <= right){
+      int mid = left + (right - left) / 2;
+      // So we firstly try to catch the null mid
+      if(dict.get(mid) == null || dict.get(mid) > target){
+        right = mid - 1;
+      } else if (dict.get(mid) < target){
+        left = mid + 1;
+      } else {
+        return mid;
+      }
+    }
+    return -1;
+  }
+}
+
+
+
   
   private int binarySearch(Dictionary dict, int target, int left, int right){
     while(left <= right){
