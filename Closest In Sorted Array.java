cb22c31002a80b1330969beceb418ae231daed88
Lili Yu <liliyu3@hotmail.com>
2017-12-12 20:10:52 -0500

    Update Closest In Sorted Array.java

--- Closest In Sorted Array.java
+++ Closest In Sorted Array.java
@@ -8,32 +8,32 @@ l m   r
 1 3 3 4
 l r
 |1-2| compares with |3-2|
+
+
+time = O(log(n))
+space = O(1)
 */
 
 public class Solution {
   public int closest(int[] array, int target) {
     // Write your solution here
-    if(array == null || array.length == 0){
+    if (array == null ||  array.length == 0) {
       return -1;
     }
     
     int left = 0;
     int right = array.length - 1;
-    while (left + 1 < right){
+    while (left + 1 < right) {
       int mid = left + (right - left) / 2;
-      if (array[mid] == target){
+      if (array[mid] == target) {
         return mid;
-      } else if (array[mid] < target){
+      } else if (array[mid] < target) {
         left = mid;
       } else {
         right = mid;
       }
     }
     
-    if (Math.abs(array[left] - target) > Math.abs(array[right] - target)) {
-      return right;
-    } else {
-      return left;
-    }
+    return (target - array[left]) <= (array[right] - target) ? left : right;
   }
 }
