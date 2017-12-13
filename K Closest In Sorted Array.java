f8ab37673dd6125ee3599a021da51018b3d8f2cd
Lili Yu <liliyu3@hotmail.com>
2017-12-12 22:08:53 -0500

    Update K Closest In Sorted Array.java

--- K Closest In Sorted Array.java
+++ K Closest In Sorted Array.java
@@ -22,49 +22,48 @@ l   r
 l     r
 l = -1 < 0 so just move the right side 6 in the array
 
+
+time = O(log(n) + k)
+space = O(k)
 */
 
 public class Solution {
   public int[] kClosest(int[] array, int target, int k) {
     // Write your solution here
-    if (array == null || array.length == 0){
+    if (array == null || array.length == 0) {
       return array;
     }
-    if (k == 0){
+    if (k == 0) {
       return new int[0];
     }
-    int left = noMoreThan(array, target);
+    int left = smallerOrQqual(array,target);
     int right = left + 1;
-    
     int[] result = new int[k];
-    for (int i = 0; i < k; i++){
-      //right out of bound; both in the bound, left close to the target, return left
-      if (right >= array.length || left >= 0 && target - array[left] <= array[right]-target){
+    for (int i = 0; i < k; i++) {
+      if (right >= array.length || (left >= 0 && target - array[left] <= array[right] - target)) {
         result[i] = array[left--];
-      } else {  //if left < 0, right == 0, always right ++
+      } else {
         result[i] = array[right++];
       }
     }
     return result;
   }
   
-  private int noMoreThan(int[] array, int target){
+  private int smallerOrQqual(int[] array, int target) {
     int left = 0;
     int right = array.length - 1;
-    // find the number no more than target return index, if not find return -1
-    while (left + 1 < right){
+    while (left + 1 < right) {
       int mid = left + (right - left) / 2;
-      if (array[mid] <= target){
+      if (array[mid] <= target) {
         left = mid;
       } else {
         right = mid;
       }
     }
     
-    if(array[right] <= target){
+    if (array[right] <= target) {
       return right;
-    }
-    if (array[left] <= target){
+    } else if (array[left] <= target) {
       return left;
     }
     return -1;
