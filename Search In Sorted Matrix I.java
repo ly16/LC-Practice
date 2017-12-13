fff8d79de69d84e9273150acf241cf644ff0f980
Lili Yu <liliyu3@hotmail.com>
2017-12-12 23:21:57 -0500

    Update Search In Sorted Matrix I.java

--- Search In Sorted Matrix I.java
+++ Search In Sorted Matrix I.java
@@ -34,34 +34,37 @@ m    r
 4 5 7
     lr
 8 9 10
+
+
+time = O(log(row*col))
+space = O(1)
 */
 
 
 public class Solution {
   public int[] search(int[][] matrix, int target) {
     // Write your solution here.
-    if(matrix.length == 0 || matrix[0].length == 0){
+    if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
       return new int[] {-1, -1};
     }
     
-    // convert 2D to 1D array
-    int row = matrix.length;
-    int col = matrix[0].length;
+    int rows = matrix.length;
+    int cols =  matrix[0].length;
     int left = 0;
-    int right = row * col - 1;
+    int right = rows * cols - 1;
     
-    while (left <= right){
+    while (left <= right) {
       int mid = left + (right - left) / 2;
-      int midr = mid / col;
-      int midc = mid % col;
-      if (matrix[midr][midc] == target){
-        return new int[] {midr, midc};
-      } else if (matrix[midr][midc] < target){
-        left = mid + 1;
-      } else {
+      int row = mid / cols;
+      int col = mid % cols;
+      if (matrix[row][col] == target) {
+        return new int[] {row, col};
+      } else if (matrix[row][col] > target) {
         right = mid - 1;
+      } else {
+        left = mid + 1;
       }
     }
-   return new int[] {-1, -1};
+    return new int[] {-1, -1};
   }
 }
