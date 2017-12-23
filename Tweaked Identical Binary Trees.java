0018749f4d4603242dd72e888fa4757f6786dfb3
Lili Yu <liliyu3@hotmail.com>
2017-12-23 00:01:31 -0500

    Update Tweaked Identical Binary Trees.java

--- Tweaked Identical Binary Trees.java
+++ Tweaked Identical Binary Trees.java
@@ -27,8 +27,8 @@ and
           1     4
 
 the two binary trees are tweaked identical.
-time = 4^height = 4^log_2(n) = O(n^2) for balanced
-time = 4^height = 4^n = O(2^n) for unbalanced
+time = O(n)
+space = O(height) = O(n)
 */
 
 
@@ -45,15 +45,17 @@ time = 4^height = 4^n = O(2^n) for unbalanced
 public class Solution {
   public boolean isTweakedIdentical(TreeNode one, TreeNode two) {
     // Write your solution here.
-    if(one == null && two == null){
+    if (one == null && two == null) {
       return true;
-    } else if (one ==null || two == null){
+    } else if (one == null || two == null){
       return false;
-    } else if (one.key != two.key){
+    } else if (one.key != two.key) {
       return false;
     }
-    // 2 conditions: identity or Tweaked Identical
-    return isTweakedIdentical(one.left, two.left) && isTweakedIdentical(one.right, two.right)
-    || isTweakedIdentical(one.left, two.right) && isTweakedIdentical(one.right, two.left);
+    
+    return (isTweakedIdentical(one.left, two.left)  
+    && isTweakedIdentical(one.right, two.right)) ||
+    (isTweakedIdentical(one.left, two.right) 
+    && isTweakedIdentical(one.right, two.left));
   }
 }
