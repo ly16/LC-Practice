af148bb1cbe7498ff1a675b1c6bdd9895c7cbf46
Lili Yu <liliyu3@hotmail.com>
2017-12-25 14:52:31 -0500

    Update K Smallest In Unsorted Array.java

--- K Smallest In Unsorted Array.java
+++ K Smallest In Unsorted Array.java
@@ -5,40 +5,37 @@ Use maxHeap can online(real-time) sorting and reduce space complexity when n is 
 1. heapify O(n)
 2. offer n-k elements to the maxHeap (n-k)logk
 time = O(n + (n-k)logk)
+space = O(k)      // for extra max heap
 */
 
 public class Solution {
   public int[] kSmallest(int[] array, int k) {
     // Write your solution here
-    if(array.length == 0 || k == 0){
+    if (array.length == 0 || k == 0) {
       return new int[0];
     }
-    // definetion of priorityQueue
-    PriorityQueue<Integer> maxHeap = new PriorityQueue<>(k, new Comparator<Integer>(){
-      @Override
-      public int compare(Integer o1, Integer o2){
-        // not ==, because Integer is object
-        if(o1.equals(o2)){
-          return 0;
-        }
-        return o1 > o2 ? -1 : 1;
-      }
-    });
-    
-    for (int i = 0; i < array.length; i++){
-      if(i < k){  // first k elements
-        maxHeap.offer(array[i]);
-      } else if (array[i] < maxHeap.peek()){
-        maxHeap.poll();
-        maxHeap.offer(array[i]);
-      }
+  PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(k, new Comparator<Integer>(){
+    @Override
+    public int compare(Integer o1, Integer o2) {
+      if (o1.equals(o2)) {
+        return 0;
+      } 
+      return o1 > o2 ? -1 : 1;
+    }
+  });
+    for (int i = 0; i < array.length; i++) {
+     if (i < k) {
+       maxHeap.offer(array[i]);
+     } else if (array[i] < maxHeap.peek()) {
+       maxHeap.poll();
+       maxHeap.offer(array[i]);
+     }
     }
     
     int[] result = new int[k];
-    for (int i = k - 1; i >= 0; i--){ //ascending order
+    for (int i = k - 1; i >= 0; i--) {
       result[i] = maxHeap.poll();
     }
     return result;
   }
-    
 }
