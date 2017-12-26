a32c1f54254abe45c554c513be16c0ba3b90a0f3
Lili Yu <liliyu3@hotmail.com>
2017-12-25 19:12:02 -0500

    Update Kth Smallest Number In Sorted Matrix.java

--- Kth Smallest Number In Sorted Matrix.java
+++ Kth Smallest Number In Sorted Matrix.java
@@ -3,6 +3,7 @@
 Given a matrix of size N x M. For each row the elements are sorted in ascending order, 
 and for each column the elements are also sorted in ascending order. Find the Kth smallest number in it.
 time = O(klogk) kth smallest
+space = O(k)  //extra minheap
 */
 
 public class Solution {
@@ -10,49 +11,42 @@ public class Solution {
     // Write your solution here.
     int rows = matrix.length;
     int cols = matrix[0].length;
-    
-    // Best First Seach, minHeap, definition
-    PriorityQueue<Cell> minHeap = new PriorityQueue<>(k, new Comparator<Cell>(){
+    PriorityQueue<Cell> minHeap = new PriorityQueue<Cell>(k, new Comparator<Cell>(){
       @Override
-      public int compare(Cell c1, Cell c2){
-        if (c1.value == c2.value){
+      public int compare(Cell o1, Cell o2) {
+        if (o1.value == o2.value) {
           return 0;
         }
-        return c1.value < c2.value ? -1 : 1;
+        return o1.value < o2.value ? -1 : 1;
       }
     });
-    
-    // generated to be true
     boolean[][] visited = new boolean[rows][cols];
     minHeap.offer(new Cell(0, 0, matrix[0][0]));
     visited[0][0] = true;
     
-    // Best First Search
-    for(int i = 0; i< k - 1; i++){  // poll k-1 elemets smaller, peek kth 
-      Cell cur = minHeap.poll();    
-      // not boundary or generated before
-      // check row
-      if(cur.row + 1 < rows && !visited[cur.row + 1][cur.column]){
-      minHeap.offer(new Cell(cur.row + 1, cur.column, matrix[cur.row + 1][cur.column]));
-      visited[cur.row + 1][cur.column] = true;
+    for(int i = 0; i < k - 1; i++) {
+      Cell cur = minHeap.poll();
+      if (cur.row + 1 < rows && !visited[cur.row + 1][cur.col]) {
+        minHeap.offer(new Cell(cur.row + 1, cur.col, matrix[cur.row + 1][cur.col]));
+        visited[cur.row + 1][cur.col] = true;
       }
-      // check column
-      if(cur.column + 1 < cols && !visited[cur.row][cur.column + 1]){
-      minHeap.offer(new Cell(cur.row, cur.column + 1, matrix[cur.row][cur.column + 1]));
-      visited[cur.row][cur.column + 1] = true;
+      if (cur.col + 1 < cols && !visited[cur.row][cur.col + 1]) {
+        minHeap.offer(new Cell(cur.row, cur.col + 1, matrix[cur.row][cur.col + 1]));
+        visited[cur.row][cur.col + 1] = true;
       }
     }
+    
     return minHeap.peek().value;
   }
   
   static class Cell{
     int row;
-    int column;
+    int col;
     int value;
     
-    Cell(int row, int column, int value){
+    Cell(int row, int col, int value) {
       this.row = row;
-      this.column = column; 
+      this.col = col;
       this.value = value;
     }
   }
