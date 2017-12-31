797e878f76fffd59b78fd21e63d95b826ca2e9b3
Lili Yu <liliyu3@hotmail.com>
2017-12-30 23:31:10 -0500

    Update Remove Spaces.java

--- Remove Spaces.java
+++ Remove Spaces.java
@@ -13,24 +13,19 @@ time = O(n), space = O(1)
 public class Solution {
   public String removeSpaces(String input) {
     // Write your solution here.
-    if (input.isEmpty()) {  //.isEmpty()
-      return input;
-    }
     char[] array = input.toCharArray();
-    int end = 0;  // not include end as answer
-    for (int i = 0; i< array.length; i++) {
-      if (array[i] == ' ' && (i == 0 || array[i - 1] == ' ')) {
+    int slow = 0;
+    for (int fast = 0; fast < array.length; fast++) {
+      if (array[fast] == ' ' && (fast == 0 || array[fast - 1] == ' ')) {
         continue;
       }
-      array[end++] = array[i];
+      array[slow++] = array[fast];
     }
-    
     // space at the end
-    if (end > 0 && array[end - 1] == ' ') {
-      return new String(array, 0, end - 1); // char array
+    if (slow > 0 && array[slow - 1] == ' ') {
+      return new String(array, 0, slow - 1);
     }
-    return new String(array, 0, end);
+    return new String(array, 0, slow);
   }
 }
-
 
