deddd26ed09db7474c76c9f7885e94fbff51e41b
Lili Yu <liliyu3@hotmail.com>
2017-12-31 00:32:34 -0500

    Update Determine If One String Is Another's Substring.java

--- Determine If One String Is Another's Substring.java
+++ Determine If One String Is Another's Substring.java
@@ -17,28 +17,26 @@ space = O(1)
 public class Solution {
   public int strstr(String large, String small) {
     // write your solution here
+    if (small.isEmpty()) {
+      return 0;
+    }
     if (large.length() < small.length()) {
       return -1;
     }
-    if (small.length() == 0) {
-      return 0;
-    }
-    
-   for (int i = 0; i <= large.length() - small.length(); i++) { //"=" we should ensure we can iterate the whole small length
-     if (equals(large, i, small)) {
-       return i;
-     }
-   }
-      return -1;
-  }
-  
-  public boolean equals(String large, int start, String small) {
-    for (int i = 0; i < small.length(); i++) {
-      if (large.charAt(i + start) != small.charAt(i)) {
-        return false;
+    for (int i = 0; i <= large.length() - small.length(); i++) {
+      int k = i;
+      int j = 0;
+      for (j = 0; j < small.length(); j++) {
+        if (large.charAt(k) != small.charAt(j)) {
+          break;
+        }
+        k++;
+      }
+      if (j == small.length()) {
+        return i;
       }
     }
-    return true;
+    return -1;
   }
-  
 }
+
