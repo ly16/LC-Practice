197081679c2de5ea0aef71be109a487d5bafaef0
Lili Yu <liliyu3@hotmail.com>
2018-01-03 16:34:56 -0500

    Update Reverse String.java

--- Reverse String.java
+++ Reverse String.java
@@ -27,22 +27,19 @@ space = O(1)
 public class Solution {
   public String reverse(String input) {
     // Write your solution here.
-    if (input == null || input.length() <= 1) {
+    if (input.length() <= 1) {
       return input;
     }
-    
     char[] array = input.toCharArray();
-    for (int left = 0, right = array.length - 1; left < right; left++, right--) {
-      swap(array, left, right);
+    int left = 0;
+    int right = array.length - 1;
+    while (left < right) {
+      char temp = array[left];
+      array[left] = array[right];
+      array[right] = temp;
+      left++;
+      right--;
     }
-    
     return new String(array);
   }
-  
-  private void swap(char[] array, int left, int right) {
-    char temp = array[left];
-    array[left] = array[right];
-    array[right] = temp;
-  }
-  
 }
