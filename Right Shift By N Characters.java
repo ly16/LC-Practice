37346558666dfa8fdde1d6877cf1eae457aa35c1
Lili Yu <liliyu3@hotmail.com>
2018-01-03 20:38:57 -0500

    Update Right Shift By N Characters.java

--- Right Shift By N Characters.java
+++ Right Shift By N Characters.java
@@ -9,37 +9,39 @@ cdeab
 
 return "cdeab"
 
+assumption:
+The given string is not null.
+n >= 0.
+
 time = O(n)
 space = O(1)
 */
 
+
 public class Solution {
   public String rightShift(String input, int n) {
-       // Write your solution here.
-      if (input == null || input.length() <= 1) {
-          return input;
-      }
-      char[] array = input.toCharArray();
-      n %= array.length;  //n = n % array.length, 3 % 5 = 23 % 5
-      // right part
-      reverse(array, array.length - n, array.length - 1);
-      // left part
-      reverse(array, 0, array.length - n - 1);
-      // whole 
-      reverse(array, 0 ,array.length - 1);
-      return new String(array);
+    // Write your solution here.
+    if (input.length() <= 1) {
+      return input;
+    }
+    char[] array = input.toCharArray();
+    n = n % array.length;
+    reverse(array, 0, array.length - n - 1);
+    reverse(array, array.length - n, array.length - 1);
+    reverse(array, 0, array.length - 1);
+    return new String(array);
   }
-
   private void reverse(char[] array, int left, int right) {
-    while (left < right) {
-        char temp = array[left];
-        array[left] = array[right];
-        array[right] = temp;
-        left++;
-        right--;
+    while(left < right) {
+      char temp = array[left];
+      array[left] = array[right];
+      array[right] = temp;
+      left++;
+      right--;
     }
   }
 }
+
 
 
 
