baadc89f69b595cbf5288583816913e026df37fb
Lili Yu <liliyu3@hotmail.com>
2018-01-07 23:13:15 -0500

    Update Spiral Order Traverse I.java

--- Spiral Order Traverse I.java
+++ Spiral Order Traverse I.java
@@ -21,36 +21,36 @@ space = O(n)
 public class Solution {
   public List<Integer> spiral(int[][] matrix) {
     // Write your solution here.
-    List<Integer> result = new ArrayList<>();
+    List<Integer> result = new ArrayList<Integer>();
     if (matrix.length == 0) {
       return result;
     }
     if (matrix[0].length == 0) {
       return result;
     }
-    traverse(matrix, 0, matrix.length, result);
+    int start = 0;
+    int end = matrix.length - 1;
+    
+    while (start < end) {
+      for (int i = start; i < end; i++) {
+        result.add(matrix[start][i]);
+      }
+      for (int i = start; i < end; i++) {
+        result.add(matrix[i][end]);
+      }
+      for (int i = end; i > start; i--) {
+        result.add(matrix[end][i]);
+      }
+      for (int i = end; i > start; i--) {
+        result.add(matrix[i][start]);
+      }
+      start++;
+      end--;
+    }
+    if (start == end) {
+      result.add(matrix[end][end]);
+    }
     return result;
   }
-  private void traverse(int[][] matrix, int offset, int size, List<Integer> result) {
-    if (size == 0) {
-      return;
-    }
-    if (size == 1) {
-      result.add(matrix[offset][offset]);
-      return;
-    }
-    for (int i = 0; i < size - 1; i++) {
-      result.add(matrix[offset][offset + i]);
-    }
-    for (int i = 0; i < size - 1; i++) {
-      result.add(matrix[offset + i][offset + size - 1]);
-    }
-    for (int i = size - 1; i > 0; i--) {
-      result.add(matrix[offset + size - 1][offset + i]);
-    }
-    for (int i = size - 1; i > 0; i--) {
-      result.add(matrix[offset + i][offset]);
-    }
-    traverse(matrix, offset + 1, size - 2, result);
-  }
 }
+
