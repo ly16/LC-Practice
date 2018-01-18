67db789c91ff1dfa8e25acd7b79cd4134d38328b
Lilly Yu <liliyu3@hotmail.com>
2018-01-17 22:40:09 -0500

    Create  Move 0s To The End II.java

---
+++ Move 0s To The End II.java
@@ -0,0 +1,35 @@
+/*
+Given an array of integers, move all the 0s to the right end of the array.
+The relative order of the elements in the original array need to be maintained.
+
+Assumptions:
+The given array is not null.
+
+Examples:
+{1} --> {1}
+{1, 0, 3, 0, 1} --> {1, 3, 1, 0, 0}
+
+time = O(n)
+space = O(1)
+*/
+
+public class Solution {
+  public int[] moveZero(int[] array) {
+    // Write your solution here.
+    if (array == null || array.length <= 1) {
+      return array;
+    }
+    int slow = 0;
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] != 0) {
+        array[slow++] = array[i];
+      }
+    }
+    
+    while (slow < array.length) {
+      array[slow++] = 0;
+    }
+    
+    return array;
+  }
+}
