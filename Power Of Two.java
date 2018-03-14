45b93509d4e8f5db29a6882fa57dd67e50ecf330
Lilly Yu <liliyu3@hotmail.com>
2018-03-13 22:25:54 -0400

    Create Power Of Two.java

---
+++ Power Of Two.java
@@ -0,0 +1,25 @@
+/*
+Determine if a given integer is power of 2.
+
+Examples
+16 is power of 2 (2 ^ 4)
+3 is not
+0 is not
+-1 is not
+
+time = O(1)
+space = O(1)
+*/
+
+public class Solution {
+  public boolean isPowerOfTwo(int number) {
+    // Write your solution here.
+  	if (number <= 0) {
+    	return false;
+    }
+    while ((number & 1) == 0) {
+      number >>>= 1;
+    }
+    return number == 1;
+  }
+}
