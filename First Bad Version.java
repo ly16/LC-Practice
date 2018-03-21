fd3c6cfd60478b6b15d77d21d5eca6145884a6c4
Lilly Yu <liliyu3@hotmail.com>
2018-03-20 21:10:28 -0400

    Create First Bad Version.java

---
+++ First Bad Version.java
@@ -0,0 +1,47 @@
+/*
+The code base version is an integer start from 1 to n. One day, someone committed a bad version in the code case, 
+so it caused this version and the following versions are all failed in the unit tests. Find the first bad version.
+
+You can call isBadVersion to help you determine which version is the first bad one. 
+The details interface can be found in the code's annotation part.
+
+Example
+Given n = 5:
+isBadVersion(3) -> false
+isBadVersion(5) -> true
+isBadVersion(4) -> true
+Here we are 100% sure that the 4th version is the first bad version.
+
+time = O(logn)
+space = O(1)
+*/
+
+
+
+/**
+ * public class SVNRepo {
+ *     public static boolean isBadVersion(int k);
+ * }
+ * you can use SVNRepo.isBadVersion(k) to judge whether 
+ * the kth code version is bad or not.
+*/
+
+public class Solution {
+    public int findFirstBadVersion(int n) {
+        // first occurance
+        // time = O(logn), space = O(1)
+        
+        int left = 1;
+        int right = n;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (SVNRepo.isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+        
+        return SVNRepo.isBadVersion(left) ? left : right;
+    }
+}
