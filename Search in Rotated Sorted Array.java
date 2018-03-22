3f38f4d47c00457d322a0b050d25e774fa7238c1
Lilly Yu <liliyu3@hotmail.com>
2018-03-22 00:06:26 -0400

    Create Search in Rotated Sorted Array.java

---
+++ Search in Rotated Sorted Array.java
@@ -0,0 +1,48 @@
+/*
+Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+You may assume no duplicate exists in the array.
+
+Example
+For [4, 5, 1, 2, 3] and target=1, return 2.
+For [4, 5, 1, 2, 3] and target=0, return -1.
+
+time = O(logn)
+space = O(1)
+*/
+
+
+public class Solution {
+    public int search(int[] A, int target) {
+        // seperate into two parts, two ascending parts
+        // time = O(logn), space = O(1)
+        if (A == null || A.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = A.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (A[mid] == target) {
+                return mid;
+            } 
+            // first ascending part
+            if (A[mid] > A[left]) {
+                if (target >= A[left] && target < A[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                // second ascending part, right side of pivot
+                if (A[mid] < target && target <= A[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
