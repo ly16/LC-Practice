b976fc02dcb01cd5a93885145909cca95a725aae
Lilly Yu <liliyu3@hotmail.com>
2018-03-23 14:52:49 -0400

    Update Prime Factorization.java

--- Prime Factorization.java
+++ Prime Factorization.java
@@ -1,30 +1,32 @@
 /*
 Prime factorize a given integer.
+
+Example
 Given 10, return [2, 5].
 Given 660, return [2, 2, 3, 5, 11].
 
+time = O(sqrt(n))
+space = O(logn) for the new list
 */
 
+
 public class Solution {
-    /**
-     * @param num an integer
-     * @return an integer array
-     */
     public List<Integer> primeFactorization(int num) {
-        // Write your code here
-        List<Integer> factor= new ArrayList(); 
-        
-        for(int i=2; i*i<=num; i++){
-            while(num%i ==0 ){
-                num=num/i;
-                factor.add(i);
+        // time = O(sqrt(n)), space = O(logn) for the result list
+        List<Integer> result = new ArrayList<>();
+        if (num <= 1) {
+            return result;
+        }
+        int upper = (int) Math.sqrt(num);
+        for (int i = 2; i <= upper && i > 1; i++) {
+            while (num % i == 0) {
+                num /= i;
+                result.add(i);
             }
         }
-        //prime num
-        if(num!=1){
-            factor.add(num);
+        if (num > 1) {
+            result.add(num);
         }
-        
-        return factor;
+        return result;
     }
 }
