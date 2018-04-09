92fab3ea1713fba64b8e800f9e307ee8c57b2ec4
Lilly Yu <liliyu3@hotmail.com>
2018-04-09 00:09:07 -0400

    Update Permutations II.java

--- Permutations II.java
+++ Permutations II.java
@@ -51,4 +51,45 @@ public class Solution {
             curLayer.remove(curLayer.size() - 1);
         }    
                      }
-};
+}
+
+
+// version 2 (better):
+public class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        // write your code here
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null) {
+            return result;
+        }
+        dfs(nums, result, 0);
+        return result;
+    }
+    
+    private void dfs(int[] nums,List<List<Integer>> result, int index) {
+        if (index == nums.length) {
+            List<Integer> curList = new ArrayList<>();
+            for (int num : nums) {
+                curList.add(num);
+            }
+            result.add(new ArrayList<Integer>(curList));
+            return;
+        }
+        
+        // avoid duplication
+        Set<Integer> set = new HashSet<>();
+        for (int i = index; i < nums.length; i++) {
+            if (set.add(nums[i])) {
+              swap(nums, index, i);
+              dfs(nums, result, index + 1);
+              swap(nums, index, i);
+            }
+        }
+    }
+    
+    private void swap(int[] nums, int left, int right) {
+        int tmp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = tmp;
+    }
+}
