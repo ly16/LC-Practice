913a6effb82557ccbf4097824e7e8da20c83d8b9
Lilly Yu <liliyu3@hotmail.com>
2018-04-13 00:03:06 -0400

    Create Minimum Subtree.java

---
+++ Minimum Subtree.java
@@ -0,0 +1,48 @@
+/*
+Given a binary tree, find the subtree with minimum sum. Return the root of the subtree.
+
+Example
+Given a binary tree:
+     1
+   /   \
+ -5     2
+ / \   /  \
+0   2 -4  -5 
+return the node 1.
+
+time = O(n)
+space = O(height) = O(n)
+*/
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    private TreeNode minRoot = null;
+    private int min = Integer.MAX_VALUE; 
+    public TreeNode findSubtree(TreeNode root) {
+        // write your code here
+        helper(root);
+        return minRoot;
+    }
+    private int helper(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        int sum = helper(root.left) + helper(root.right) + root.val;
+        if (sum < min) {
+            min = sum;
+            minRoot = root;
+        }
+        return sum;
+    }
+}
