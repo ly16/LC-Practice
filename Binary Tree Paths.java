1c69e56a3b9671f6f9266f132f0fcc6323cf8b4c
Lilly Yu <liliyu3@hotmail.com>
2018-04-12 23:14:13 -0400

    Update Binary Tree Paths.java

--- Binary Tree Paths.java
+++ Binary Tree Paths.java
@@ -11,6 +11,9 @@ All root-to-leaf paths are:
   "1->2->5",
   "1->3"
 ]
+
+time = O(n)
+space = O(height) = O(n)
 */
 
 /**
@@ -24,34 +27,27 @@ All root-to-leaf paths are:
  *     }
  * }
  */
+
 public class Solution {
-    /**
-     * @param root the root of the binary tree
-     * @return all root-to-leaf paths
-     */
     public List<String> binaryTreePaths(TreeNode root) {
-        // Write your code here
-        List<String> result= new ArrayList<String>();
-        if(root==null){
+        // write your code here
+        List<String> result = new ArrayList<>();
+        if (root == null) {
             return result;
         }
-        helper(root, String.valueOf(root.val), result);
+        helper(root, "", result);
         return result;
     }
-    private void helper(TreeNode root, String path, List <String> result){
-        if(root==null){
+    private void helper(TreeNode root, String path, List<String> result) {
+        if (root.left == null && root.right == null) {
+            result.add(path + root.val);
             return;
         }
-        if(root.left==null && root.right==null){
-            result.add(path);
-            return;
+        if (root.left != null) {
+            helper(root.left, path + root.val + "->", result);
         }
-        if(root.left!=null){
-            helper(root.left, path+"->"+String.valueOf(root.left.val), result);
+        if (root.right != null) {
+            helper(root.right, path + root.val + "->", result);
         }
-        if(root.right!=null){
-            helper(root.right, path+"->"+String.valueOf(root.right.val), result);
-        }
-        
     }
 }
