6ec1e8f850e0a0ad123baf4ba464594510de7b6c
Lilly Yu <liliyu3@hotmail.com>
2018-05-20 00:40:59 -0400

    Create Sort List.java

---
+++ Sort List.java
@@ -0,0 +1,70 @@
+/*
+Sort a linked list in O(nlogn) time using constant space complexity.
+
+Example 1:
+Input: 4->2->1->3
+Output: 1->2->3->4
+
+Example 2:
+Input: -1->5->3->4->0
+Output: -1->0->3->4->5
+
+time = O(nlogn)
+space = O(n)  for the new list
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    // use merge sort with O(nlogn)
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        ListNode mid = findMid(head);
+        ListNode right = sortList(mid.next);
+        mid.next = null;
+        ListNode left = sortList(head);
+        return merge(left, right);
+    }
+    
+    private ListNode findMid(ListNode head) {
+        ListNode slow = head, fast =  head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    
+    private ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        
+        while (head1 != null && head2 != null) {
+            if (head1.val < head2.val) {
+                cur.next = head1;
+                head1 = head1.next;
+            } else {
+                cur.next = head2;
+                head2 = head2.next;
+            }
+            cur = cur.next;
+        }
+        
+        if (head1 != null) {
+            cur.next = head1;
+        } else if (head2 != null) {
+            cur.next = head2;
+        }
+        
+        return dummy.next;
+    }
+}
