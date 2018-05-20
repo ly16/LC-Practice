325ae076c79d2983cfa3ed11c9b1df685c66ab71
Lilly Yu <liliyu3@hotmail.com>
2018-05-20 06:18:09 -0400

    Update Search a 2D Matrix II.java

--- Search a 2D Matrix II.java
+++ Search a 2D Matrix II.java
@@ -16,10 +16,11 @@ Consider the following matrix:
 ]
 Given target = 3, return 2.
 
-time = O(n + m)
+time = O(nm)
 space = O(1)
 */
 
+// lintcode version
 public class Solution {
     public int searchMatrix(int[][] matrix, int target) {
         // linear of length and width
@@ -49,5 +50,33 @@ public class Solution {
             }
         }
         return count;
+    }
+}
+
+
+// leetcode version
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+        if (matrix[0] == null || matrix[0].length == 0) {
+            return false;
+        }
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int row = rows - 1;
+        int col = 0;
+        
+        while (row >= 0 && col < cols) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                col++;
+            } else {
+                row--;
+            }
+        }
+        return false;
     }
 }
