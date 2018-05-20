a0e67be5c42f738d0ecfebe6da46668b70655573
Lilly Yu <liliyu3@hotmail.com>
2018-05-20 05:44:34 -0400

    Create Search for a Range.java

---
+++ Search for a Range.java
@@ -0,0 +1,65 @@
+/*
+Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+Your algorithm's runtime complexity must be in the order of O(log n).
+If the target is not found in the array, return [-1, -1].
+
+Example 1:
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+
+Example 2:
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+
+time = O(logn)
+space = O(1)
+*/
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return new int[]{-1, -1};
+        }
+        int left = findFirst(nums, target);
+        int right = findLast(nums, target);
+        return new int[]{left, right};
+    }
+    
+    // first occurence
+    private int findFirst(int[] nums, int target) {
+        int left = 0, right = nums.length;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+        if (nums[left] == target) {
+            return left;
+        } else if (nums[right] == target) {
+            return right;
+        }
+        return -1;
+    }
+    
+    // last occurence
+    private int findLast(int[] nums, int target) {
+        int left = 0, right = nums.length;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        if (nums[right] == target) {
+            return right;
+        } else if (nums[left] == target) {
+            return left;
+        }
+        return -1;
+    }
+}
