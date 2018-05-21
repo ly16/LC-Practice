1ce2fa245c38a5733236052790bf92e0a7190acc
Lilly Yu <liliyu3@hotmail.com>
2018-05-21 10:44:33 -0400

    Update Move 0s To The End II.java

--- Move 0s To The End II.java
+++ Move 0s To The End II.java
@@ -33,3 +33,24 @@ public class Solution {
     return array;
   }
 }
+
+
+// leetcode version
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        int slow = 0, fast = 0;
+        while (fast < nums.length) {
+            if (nums[fast] == 0) {
+                fast++;
+            } else {
+                nums[slow++] = nums[fast++];
+            }
+        }
+        while (slow < nums.length) {
+            nums[slow++] = 0;
+        }
+    }
+}
