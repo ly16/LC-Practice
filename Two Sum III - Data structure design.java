8323b2f07037c2c2d58335fbe755bacf15c8b5ea
Lilly Yu <liliyu3@hotmail.com>
2018-05-21 10:28:11 -0400

    Create Two Sum III - Data structure design.java

---
+++ Two Sum III - Data structure design.java
@@ -0,0 +1,57 @@
+/*
+Design and implement a TwoSum class. It should support the following operations: add and find.
+add - Add the number to an internal data structure.
+find - Find if there exists any pair of numbers which sum is equal to the value.
+
+Example 1:
+add(1); add(3); add(5);
+find(4) -> true
+find(7) -> false
+
+Example 2:
+add(3); add(1); add(2);
+find(3) -> true
+find(6) -> false
+
+time = O(n)
+space = O(n)
+*/
+
+class TwoSum {
+
+    /** Initialize your data structure here. */
+    private List<Integer> list = new ArrayList<>();
+    private Map<Integer, Integer> map = new HashMap<>();
+    public TwoSum() {
+    
+    }
+    
+    /** Add the number to an internal data structure.. */
+    public void add(int number) {
+        if (map.containsKey(number)) {
+            map.put(number, map.get(number) + 1);
+        } else {
+            map.put(number, 1);
+            list.add(number);
+        }
+    }
+    
+    /** Find if there exists any pair of numbers which sum is equal to the value. */
+    public boolean find(int value) {
+        for (int i = 0; i < list.size(); i++) {
+            int num = list.get(i);
+            int diff = value - num;
+            if ((num == diff && map.get(num) > 1) ||(num != diff && map.containsKey(diff))) {
+                return true;
+            }
+        }
+            return false;
+    }
+}
+
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum obj = new TwoSum();
+ * obj.add(number);
+ * boolean param_2 = obj.find(value);
+ */
