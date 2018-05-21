6df0419133f1ede50d56ec3d40e98e3599b62c45
Lilly Yu <liliyu3@hotmail.com>
2018-05-21 02:47:53 -0400

    Update Longest Ascending Subsequence.java

--- Longest Ascending Subsequence.java
+++ Longest Ascending Subsequence.java
@@ -9,10 +9,17 @@ Input: A = {5, 2, 6, 3, 4, 7, 5}
 Output: 4
 Because [2, 3, 4, 5] is the longest ascending subsequence.
 
+binary:
 time = O(nlogn)
 space = O(n)  for the int[]
+
+dp:
+time = O(n^2)
+space = O(n) for the int[]
 */
 
+
+// binary version
 public class Solution {
   public int longest(int[] array) {
     // Write your solution here.
@@ -44,4 +51,29 @@ public class Solution {
     }
     return right;
   }
+}
+
+
+// dp version:
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        // dp with O(n^2), space = O(n)
+        int max = 0;
+        if (nums == null || nums.length == 0) {
+            return max;
+        }
+        int[] record = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            record[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    record[i] = record[i] > record[j] + 1 ? record[i] : record[j] + 1;
+                }
+            }
+            if (record[i] > max) {
+                max = record[i];
+            }
+        }
+        return max;
+    }
 }
