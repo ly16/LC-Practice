4962689e71b7e27214cea636e65e0eabec2adc8b
Lilly Yu <liliyu3@hotmail.com>
2018-05-24 08:33:14 -0400

    Update 4 Sum.java

--- 4 Sum.java
+++ 4 Sum.java
@@ -44,3 +44,48 @@ public class Solution {
     return false;
   }
 }
+
+
+/*
+leetcode version
+time = O(n^3)
+space = O(1)
+*/
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        // O(n^3) version
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+		Arrays.sort(nums);
+		for (int i = 0; i < nums.length - 3; i++) {
+			if (i != 0 && nums[i] == nums[i - 1]) {
+				continue;
+			}
+
+			for (int j = i + 1; j < nums.length - 2; j++) {
+				if (j != i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }   
+				int left = j + 1;
+				int right = nums.length - 1;
+				while (left < right) {
+					int sum = nums[i] + nums[j] + nums[left] + nums[right];
+					if (sum < target) {
+						left++;
+					} else if (sum > target) {
+						right--;
+					} else {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left++],nums[right--]));
+						while (left < right && nums[left] == nums[left - 1]) {
+							left++;
+						}
+						while (left < right && nums[right] == nums[right + 1]) {
+							right--;
+						}
+					}
+				}
+			}
+		}
+
+		return result;
+    }
+}
