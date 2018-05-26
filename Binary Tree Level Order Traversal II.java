2563d602672199b8b86b401dba802622b481d1c9
Lilly Yu <liliyu3@hotmail.com>
2018-05-25 23:26:51 -0400

    Create Binary Tree Level Order Traversal II.java

---
+++ Binary Tree Level Order Traversal II.java
@@ -0,0 +1,60 @@
+/*
+Given a binary tree, return the bottom-up level order traversal of its nodes' values. 
+(ie, from left to right, level by level from leaf to root).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its bottom-up level order traversal as:
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+
+
+time = O(n + n) = O(n)
+space = O(n) for the queue and list
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                level.add(cur.val);
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                } 
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+            result.add(level);
+        }
+        Collections.reverse(result);    // O(n)
+        return result;
+    }
+}
