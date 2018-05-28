8ee0b230ae6dcf2a6886753e3a5afae98991dae4
Lilly Yu <liliyu3@hotmail.com>
2018-05-28 10:40:44 -0400

    Update Binary Tree Preorder Traversal.java

--- Binary Tree Preorder Traversal.java
+++ Binary Tree Preorder Traversal.java
@@ -14,6 +14,7 @@ return [1,2,4,5,3].
 
 time = O(n)
 space = O(height) = O(n)
+Or space = O(n) for the deque
 */
 
 /**
@@ -44,3 +45,27 @@ class Solution {
     }
 }
 
+
+// iterative version:
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        if (root == null) {
+            return result;
+        }
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.offerFirst(root);
+        
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pollFirst();
+            result.add(cur.val);
+            if (cur.right != null) {
+                stack.offerFirst(cur.right);
+            }
+            if (cur.left != null) {
+                stack.offerFirst(cur.left);
+            }
+        }
+        return result;
+    }
+}
