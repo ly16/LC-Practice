fb671a646fc7dc710b155d50b3610b5e1e87aae9
Lilly Yu <liliyu3@hotmail.com>
2018-05-29 03:50:15 -0400

    Update Validate Binary Search Tree.java

--- Validate Binary Search Tree.java
+++ Validate Binary Search Tree.java
@@ -49,3 +49,34 @@ public class Solution {
         return checkBST(node.left, min, node.val - 1) && checkBST(node.right, node.val + 1, max);
     }
 }
+
+
+
+// leetcode version
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    private boolean helper(TreeNode root, long min, long max) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+    }
+}
