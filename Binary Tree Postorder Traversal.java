b92786e8f9a6211d824d4ea1f8032822ab2186bc
Lilly Yu <liliyu3@hotmail.com>
2018-05-29 03:33:25 -0400

    Update Binary Tree Postorder Traversal.java

--- Binary Tree Postorder Traversal.java
+++ Binary Tree Postorder Traversal.java
@@ -1,42 +1,74 @@
 /*
 Given a binary tree, return the postorder traversal of its nodes' values.
 
-Example
-Given binary tree {1,#,2,3},
+Example:
+Input: [1,null,2,3]
    1
     \
      2
     /
    3
+Output: [3,2,1]
 
+Follow up: Recursive solution is trivial, could you do it iteratively?
+
+time = O(n)
+space = O(height) = O(n)
 */
 
 /**
- * Definition of TreeNode:
+ * Definition for a binary tree node.
  * public class TreeNode {
- *     public int val;
- *     public TreeNode left, right;
- *     public TreeNode(int val) {
- *         this.val = val;
- *         this.left = this.right = null;
- *     }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
  * }
  */
-public class Solution {
-    /**
-     * @param root: The root of binary tree.
-     * @return: Postorder in ArrayList which contains node values.
-     */
-    public ArrayList<Integer> postorderTraversal(TreeNode root) {
-        // write your code here
-        ArrayList<Integer> result= new ArrayList<>();
-        if(root==null){
+
+// recursive version
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        if (root  == null) {
             return result;
         }
-        result.addAll(postorderTraversal(root.left));
-        result.addAll(postorderTraversal(root.right));
+        return helper(root, result);
+    }
+    private List<Integer> helper(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return result;
+        }
+        helper(root.left, result);
+        helper(root.right, result);
         result.add(root.val);
+        return result;
+    }
+}
+
+
+// iterative version
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.offerFirst(root);
         
+        while (!stack.isEmpty()) {
+            // left, right, root -> root, right, left -> root, left, right
+            TreeNode cur = stack.pollFirst();
+            result.add(cur.val);
+            if (cur.left != null) {
+                stack.offerFirst(cur.left);
+            }
+            if (cur.right != null) {
+                stack.offerFirst(cur.right);
+            }
+        }
+        Collections.reverse(result);
         return result;
     }
 }
