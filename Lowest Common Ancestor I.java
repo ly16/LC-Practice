851bf474cc9ee97e93cd6a37124be4ff7a73321f
Lilly Yu <liliyu3@hotmail.com>
2018-05-30 07:00:08 -0400

    Update Lowest Common Ancestor I.java

--- Lowest Common Ancestor I.java
+++ Lowest Common Ancestor I.java
@@ -53,3 +53,36 @@ public class Solution {
     return left == null ? right : left;
   }
 }
+
+
+// leetcode medium version
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        
+        if (left != null && right != null) {
+            return root;
+        } else if (right == null) {
+            return left;
+        } else if (left == null) {
+            return right;
+        }
+        return null;
+    }
+}
