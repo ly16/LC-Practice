d4160b0d61a9531a80644991911f88f811c34c00
Lilly Yu <liliyu3@hotmail.com>
2018-06-01 01:23:46 -0400

    Update All Subsets II.java

--- All Subsets II.java
+++ All Subsets II.java
@@ -40,3 +40,28 @@ public class Solution {
     }
   }
 }
+
+
+// leetcode version (duplicate)
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null) {
+            return null;
+        }
+        Arrays.sort(nums);
+        dfs(result, nums, new ArrayList<>(), 0);
+        return result;
+    }
+    
+    private void dfs(List<List<Integer>> result, int[] nums, List<Integer> list, int start) {
+        result.add(new ArrayList<>(list));
+        for (int i = start; i < nums.length; i++) {
+            if (i == start || nums[i - 1] != nums[i]) {
+                list.add(nums[i]);
+                dfs(result, nums, list, i + 1);
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+}
