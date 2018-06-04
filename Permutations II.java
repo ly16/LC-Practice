0961a54cf8c84bed7833c149d418a39ef2206002
Lilly Yu <liliyu3@hotmail.com>
2018-06-04 09:45:25 -0400

    Update Permutations II.java

--- Permutations II.java
+++ Permutations II.java
@@ -93,3 +93,35 @@ public class Solution {
         nums[right] = tmp;
     }
 }
+
+
+// leetcode version
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        dfs(nums, result, new ArrayList<>(), new boolean[nums.length]);
+        return result;
+    }
+    
+    private void dfs(int[] nums, List<List<Integer>> result, List<Integer> list, boolean[] visited) {
+        if (list.size() == nums.length) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            if (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1]) {
+                continue;
+            }
+            list.add(nums[i]);
+            visited[i] = true;
+            dfs(nums, result, list, visited);
+            list.remove(list.size() - 1);
+            visited[i] = false;
+        }
+    }
+}
