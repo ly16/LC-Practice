ce9f5b9dff54d9daf81b49e0fc2046096e818e8c
Lilly Yu <liliyu3@hotmail.com>
2018-06-11 12:27:08 -0400

    Update Implement LRU Cache.java

--- Implement LRU Cache.java
+++ Implement LRU Cache.java
@@ -89,3 +89,96 @@ public class Solution<K, V> {
     return node;
   }
 }
+
+
+
+// leetcode version
+class LRUCache {
+    private class Node {
+        Node prev;
+        Node next;
+        int key;
+        int value;
+        
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+        
+        void update(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+    
+    private final int capacity;
+    private Node head;
+    private Node tail;
+    private Map<Integer, Node> map;
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+    }
+    
+    
+    public int get(int key) {
+        Node node = map.get(key);
+        if (node == null) {
+            return -1;
+        }
+        remove(node);
+        append(node);
+        return node.value;
+    }
+    
+    public void append(Node node) {
+        map.put(node.key, node);
+        if (head == null) {
+            head = tail = node;
+        } else {
+            node.next = head;
+            head.prev = node;
+            head = node;
+        }
+    }
+    
+    public void remove(Node node) {
+        map.remove(node.key);
+        if (node.prev != null) {
+            node.prev.next = node.next;
+        }
+        if (node.next != null) {
+            node.next.prev = node.prev;
+        }
+        if (node == head) {
+            head = head.next;
+        }
+        if (node == tail) {
+            tail = tail.prev;
+        }
+        node.prev = node.next = null;
+    }
+    
+    public void put(int key, int value) {
+        Node node = null;
+        if (map.containsKey(key)) {
+            node = map.get(key);
+            node.value = value;
+            remove(node);
+        } else if (map.size() < capacity){
+            node = new Node(key, value);
+        } else {
+            node = tail;
+            remove(node);
+            node.update(key, value);
+        }
+        append(node);
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
