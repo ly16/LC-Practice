547f2d561f33c660993610806aed97df27853c48
Lilly Yu <liliyu3@hotmail.com>
2018-06-15 05:30:28 -0400

    Update Merge Two Sorted Linked Lists.java

--- Merge Two Sorted Linked Lists.java
+++ Merge Two Sorted Linked Lists.java
@@ -31,7 +31,7 @@ one != null
 dummy -> 1 -> 2 -> 4 -> 5 -> 6
 
 time = O(m+n)
-space = O(m+n)
+space = O(1) only dummy node
 */
 
 /**
@@ -73,4 +73,37 @@ public class Solution {
 
 
 
-
+// leetcode version
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        
+        if (l1 != null) {
+            cur.next = l1;
+        }
+        
+        if (l2 != null) {
+            cur.next = l2;
+        } 
+        return dummy.next;
+    }
+}
