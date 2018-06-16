ac95ff344c28bae54bbd21b96f34b41975590065
Lilly Yu <liliyu3@hotmail.com>
2018-06-16 10:37:08 -0400

    Update Merge K Sorted Lists.java

--- Merge K Sorted Lists.java
+++ Merge K Sorted Lists.java
@@ -4,8 +4,8 @@ Merge K sorted lists into one big sorted list in ascending order.
 Assumptions
 ListOfLists is not null, and none of the lists is null.
 
-time = O(kn*log(k))
-space = O(k)
+time = O(n*log(k)) n is the total number of nodes
+space = O(k) for the heap
 */
 
 
@@ -56,9 +56,9 @@ public class Solution {
 }
 
 
-// method2: merge in order
-// time = O(nklogk)
-// space = O(logk)
+// method2: merge sort version
+// time = O(nlogk) if large list is on the first place
+// space = O(k)
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -104,6 +104,54 @@ class Solution {
             cur.next = list1;
         } else {
             cur.next = list2;
+        }
+        
+        return dummy.next;
+    }
+}
+
+
+// version 3: merge two by two
+// time = O(nlogk), space = O(k)
+public class Solution {
+    public ListNode mergeKLists(List<ListNode> lists) {  
+        if (lists == null || lists.size() == 0) {
+            return null;
+        }
+        
+        while (lists.size() > 1) {
+            List<ListNode> new_lists = new ArrayList<ListNode>();
+            for (int i = 0; i + 1 < lists.size(); i += 2) {
+                ListNode merged_list = merge(lists.get(i), lists.get(i+1));
+                new_lists.add(merged_list);
+            }
+            if (lists.size() % 2 == 1) {
+                new_lists.add(lists.get(lists.size() - 1));
+            }
+            lists = new_lists;
+        }
+        
+        return lists.get(0);
+    }
+    
+    private ListNode merge(ListNode a, ListNode b) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while (a != null && b != null) {
+            if (a.val < b.val) {
+                tail.next = a;
+                a = a.next;
+            } else {
+                tail.next = b;
+                b = b.next;
+            }
+            tail = tail.next;
+        }
+        
+        if (a != null) {
+            tail.next = a;
+        } else {
+            tail.next = b;
         }
         
         return dummy.next;
