ef5caac5f6f3cfd4a67b9b42c538d61d726ee346
Lilly Yu <liliyu3@hotmail.com>
2018-06-17 23:27:44 -0400

    Update Merge Intervals.java

--- Merge Intervals.java
+++ Merge Intervals.java
@@ -1,57 +1,52 @@
 /*
 Given a collection of intervals, merge all overlapping intervals.
-Example
-Given intervals => merged intervals:
 
-[                     [
-  [1, 3],               [1, 6],
-  [2, 6],      =>       [8, 10],
-  [8, 10],              [15, 18]
-  [15, 18]            ]
-]
+Example 1:
+Input: [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+
+Example 2:
+Input: [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considerred overlapping.
+
+time = O(nlogn) for the sort
+space = O(n)
 */
-/**
- * Definition of Interval:
- * public class Interval {
- *     int start, end;
- *     Interval(int start, int end) {
- *         this.start = start;
- *         this.end = end;
- *     }
- */
 
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
 class Solution {
-    /**
-     * @param intervals, a collection of intervals
-     * @return: A new sorted interval list.
-     */
     public List<Interval> merge(List<Interval> intervals) {
-        // write your code here
-        if (intervals == null || intervals.size() <= 1) {
-            return intervals;
-        }
-        //sort intervals
-        Collections.sort(intervals, new IntervalComparator());
-        //add merged intervals
-        List<Interval> result= new ArrayList<Interval>();
-        Interval last = intervals.get(0);
-        for(int i=1; i<intervals.size();i++){
-            Interval curt= intervals.get(i);
-            if(curt.start<= last.end){
-                last.end=Math.max(curt.end, last.end);
-            }else{
-                result.add(last);
-                last=curt;
+        List<Interval> result = new ArrayList<>();
+        Collections.sort(intervals, new IntervalComparactor());
+        Interval last = null;
+        for (Interval item : intervals) {
+            if (last == null || last.end < item.start) {
+                result.add(item);
+                last = item;
+            } else {
+                last.end = Math.max(last.end, item.end);
             }
         }
-        result.add(last);
         return result;
     }
     
-    private class IntervalComparator implements Comparator<Interval>{
-        public int compare(Interval a, Interval b){
-            return a.start-b.start;
+    // only sort the start part
+    private class IntervalComparactor implements Comparator<Interval> {
+        public int compare(Interval o1, Interval o2) {
+            if (o1.start == o2.start) {
+                return 0;
+            } 
+            return o1.start < o2.start ? -1 : 1;
         }
     }
 }
-
