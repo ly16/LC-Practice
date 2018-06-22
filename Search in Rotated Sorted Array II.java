4eda12792a51d1bc2b43e5fbc25012491b2fae2d
Lilly Yu <liliyu3@hotmail.com>
2018-06-22 11:47:48 -0400

    Create Search in Rotated Sorted Array II.java

---
+++ Search in Rotated Sorted Array II.java
@@ -0,0 +1,52 @@
+/*
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+(i.e., [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]).
+You are given a target value to search. If found in the array return true, otherwise return false.
+
+Example 1:
+Input: nums = [2,5,6,0,0,1,2], target = 0
+Output: true
+
+Example 2:
+Input: nums = [2,5,6,0,0,1,2], target = 3
+Output: false
+
+Follow up:
+This is a follow up problem to Search in Rotated Sorted Array, where nums may contain duplicates.
+Would this affect the run-time complexity? How and why?
+
+time = O(logn) = O(n) when they are all the same
+space - O(1)
+*/
+
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return true;
+                // right part
+            } else if (nums[mid] < nums[right] || nums[mid] < nums[left]) {
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+                // left part
+            } else if (nums[mid] > nums[left] || nums[mid] > nums[right]) {
+                if (target < nums[mid] && target >= nums[left]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+            //If we get here, that means nums[start] == nums[mid] == nums[end], then shifting out
+            //any of the two sides won't change the result but can help remove duplicate from
+            //consideration, here we just use end-- but left++ works too
+                right--;
+            }
+        }
+        return false;
+    }
+}
