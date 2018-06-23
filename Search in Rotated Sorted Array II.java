573fe8f1d28acbeeb736e886d47ee878e91706f9
Lilly Yu <liliyu3@hotmail.com>
2018-06-23 09:11:17 -0400

    Update Search in Rotated Sorted Array II.java

--- Search in Rotated Sorted Array II.java
+++ Search in Rotated Sorted Array II.java
@@ -20,34 +20,30 @@ space - O(1)
 */
 
 class Solution {
-    public boolean search(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                return true;
-            }  
-            // left part, left < target < mid
-            if (nums[left] < nums[mid]) {
-                // only take = in left or right side, we already check the nums[mid]
-                if (target >= nums[left] && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-                // right part, mid < target < right
-            } else if (nums[left] > nums[mid]) {
-                if (target <= nums[right] && target > nums[mid]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
+    public int findMin(int[] nums) {
+        //  这道题目在面试中不会让写完整的程序
+        //  只需要知道最坏情况下 [1,1,1....,1] 里有一个0
+        //  这种情况使得时间复杂度必须是 O(n)
+        //  因此写一个for循环就好了 find the min value
+        //  如果你觉得，不是每个情况都是最坏情况，你想用二分法解决不是最坏情况的情况，那你就写一个二分吧。
+        //  反正面试考的不是你在这个题上会不会用二分法。这个题的考点是你想不想得到最坏情况。
+         if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < nums[end]) {
+                end = mid;
+            } else if (nums[mid] > nums[end]) {
+                start = mid;
+                // nums[mid] == nums[end], we need to de-duplilcation
             } else {
-                //If nums[start] == nums[mid], just move start to the next index. 
-                //So the worst case, that the array's elements are same, is O(n).
-                left++;
+                end--;
             }
         }
-        return false;
+        
+        return nums[start] <= nums[end] ? nums[start] : nums[end];
     }
 }
