e3a73f6f42047ba327d93011e6faa1428eac718c
Lilly Yu <liliyu3@hotmail.com>
2019-03-30 12:21:17 -0700

    Update Subtree with Maximum Average.java

--- Subtree with Maximum Average.java
+++ Subtree with Maximum Average.java
@@ -19,6 +19,13 @@ Output：11(it's a TreeNode)
 */
 
 public class Solution {
+    private class TreeNode {
+        int val;
+        TreeNode left, right;
+        TreeNode (int val) {
+            this.val = val;
+        }
+    }
     private class ResultType {
         public int sum, size;
         public ResultType(int sum, int size) {
@@ -27,34 +34,30 @@ public class Solution {
         }
     }
     
+     
+    // main function
     private TreeNode subtree = null;
     private ResultType subtreeResult = null;
-    
-    /**
-     * @param root the root of binary tree
-     * @return the root of the maximum average of subtree
-     */
-    public TreeNode findSubtree2(TreeNode root) {
+    public TreeNode findSubtree(TreeNode root) {
         helper(root);
         return subtree;
     }
+    
+     
     
     private ResultType helper(TreeNode root) {
         if (root == null) {
             return new ResultType(0, 0);
         }
-        // 分治法计算左右子树的平均值
         ResultType left = helper(root.left);
         ResultType right = helper(root.right);
-        // 当前subtree的结果是左右两颗子树的和的平均值加上自身
+         
         ResultType result = new ResultType(
             left.sum + right.sum + root.val,
             left.size + right.size + 1
         );
-        // 打擂台比较得到最大平均值的子树
-        if (subtree == null ||
-            subtreeResult.sum * result.size < result.sum * subtreeResult.size
-        ) {
+         
+        if (subtree == null || subtreeResult.sum * result.size < result.sum * subtreeResult.size) {
             subtree = root;
             subtreeResult = result;
         }
